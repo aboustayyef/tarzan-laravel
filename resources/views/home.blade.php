a3a585273565802411618167ffaa115b8684614d
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2016-11-20 20:02:14 +0000

    work in progress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,23 +9,42 @@
 	<header class="section">
 		<div class="container">
 			<div class="columns is-desktop">
-				<div class="column is-half-desktop">
-					<h1 class="title">Logo</h1>
-					<h2 class="subtitle">Providing Backbone Logistics for Ghana’s Cocoa Industry since 1979</h2>
+				<div class="column is-5-desktop">
+					<img class="mb" src="img/tarzan-logo.png" width="191" height="56" >
+					<h2 class="title is-3">Providing Backbone Logistics for Ghana’s Cocoa Industry since 1979</h2>
 				</div>
-				<div class="column is-half-desktop">
+				<div class="column is-6-desktop is-offset-1-desktop">
 					<div class="columns">
 						<div class="column is-third">
-							Services
+							<div class="tarzan_nav__header title is-4">
+								Services
+							</div>
 							<ul>
-								<li>Haulage &amp; fleet</li>
-								<li>Warehousing</li>
-								<li>Container Depot</li>
-								<li>Commercial Stores</li>
+								<a href="#"><li class="tarzan_nav__link" >Haulage &amp; fleet</li></a>
+								<a href="#"><li class="tarzan_nav__link" >Warehousing</li></a>
+								<a href="#"><li class="tarzan_nav__link" >Container </li></a>
+								<a href="#"><li class="tarzan_nav__link" >Commercial Stores</li></a>
 							</ul>
 						</div>
-						<div class="column is-third">About</div>
-						<div class="column is-third">Learn More</div>
+						<div class="column is-third">
+							<div class="tarzan_nav__header title is-4">
+								About
+							</div>
+							<ul>
+								<a href="#"><li class="tarzan_nav__link" >Location</li></a>
+								<a href="#"><li class="tarzan_nav__link" >Company &amp; History</li></a>
+								<a href="#"><li class="tarzan_nav__link" >Management</li></a>
+							</ul>
+						</div>
+						<div class="column is-third">
+							<div class="tarzan_nav__header title is-4">
+								Learn More
+							</div>
+							<ul>
+								<a href="#"><li class="tarzan_nav__link" >Our News</li></a>
+								<a href="#"><li class="tarzan_nav__link" >Contact Us</li></a>
+							</ul>
+						</div>
 					</div>
 				</div>
 			</div>
