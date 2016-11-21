fb6ad0cf4ec4d8f6d40979b61c8d3df34d1d3c7c
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2016-11-21 07:50:22 +0000

    work in progress

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,8 +10,8 @@
 		<div class="container">
 			<div class="columns is-desktop">
 				<div class="column is-5-desktop">
-					<img class="mb" src="img/tarzan-logo.png" width="191" height="56" >
-					<h2 class="title is-3">Providing Backbone Logistics for Ghana’s Cocoa Industry since 1979</h2>
+					<img class="mb2" src="img/tarzan-logo.png" width="191" height="56" >
+					<h2 class="title is-3 mb">Providing Backbone Logistics for Ghana’s Cocoa Industry since 1979</h2>
 				</div>
 				<div class="column is-6-desktop is-offset-1-desktop">
 					<div class="columns">
@@ -67,7 +67,22 @@
 			
 		</section>
 	</content>
-	<footer></footer>
+	<footer class="footer">
+	  <div class="container">
+	    <div class="content has-text-centered">
+	      <p>
+	        <strong>Bulma</strong> by <a href="http://jgthms.com">Jeremy Thomas</a>. The source code is licensed
+	        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+	        is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC ANS 4.0</a>.
+	      </p>
+	      <p>
+	        <a class="icon" href="https://github.com/jgthms/bulma">
+	          <i class="fa fa-github"></i>
+	        </a>
+	      </p>
+	    </div>
+	  </div>
+	</footer>
 
 	<script src="js/app.js"></script>
 </body>
