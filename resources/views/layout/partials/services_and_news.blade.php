d7f69aa3c0b4fe5caac51d9544c1ed59135cb919
Mustapha Hamoui <mustapha.hamoui@gmail.com>
2016-11-27 12:26:40 +0000

    work in progress

--- resources/views/layout/partials/services_and_news.blade.php
+++ resources/views/layout/partials/services_and_news.blade.php
@@ -3,6 +3,9 @@
 		<div class="columns">
 			<div class="column is-one-half-tablet is-two-thirds-desktop">
 				<div class="tarzan_services">
+					<h2 class="title" style="width:100%">
+						Our Services
+					</h2>
 					<div class="tarzan_service__tile has_right_margin has_bottom_margin" style="background-image:url('http://unsplash.it/390/100/?random')">
 						<h3 class="tarzan_service__headline">
 							Haulage &amp; Fleet
@@ -26,7 +29,10 @@
 				</div>
 			</div>
 			<div class="column is-one-half-tablet is-one-third-desktop" >
-			<h3 class="title">News</h3>
+				<h2 class="title">
+					Latest News
+				</h3>
+				<img src="http://unsplash.it/200/100/?random">
 				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere dolor nulla, deleniti dolores, veritatis aliquid, vitae at similique reiciendis alias doloribus fugiat blanditiis cupiditate suscipit in! Quae dicta atque illum.</p>
 			</div>
 		</div>
